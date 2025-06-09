856110f59226aea669f9b2134bc79bed3f268cc6
Kevin Mongare <117772705+kevmongare@users.noreply.github.com>
2025-06-09 12:26:06 +0300

    update on my python files

---
+++ _numpy.py
@@ -0,0 +1,4 @@
+import numpy as np #from numpy import array
+a= np.array([1,2,5,6])
+b =np.array([4,5,6,7])
+print(a + b)
---
+++ app.py
@@ -0,0 +1,29 @@
+# # Create list areas
+# areas = [11.25, 18.0, 20.0, 10.75, 9.50, 20.0]
+
+# # Change this command
+# areas_copy = list(areas)
+# #use of list enables one to create a totally different lst out of the areas list 
+# # Change areas_copy
+# areas_copy[0] = 5.0
+
+# # Print areas
+# # print(areas)
+# k=max(areas)#here we are working on function functions are reusable codes that help do a task  for example the max helps check the maximum value and out put it
+
+# print(k)
+# m= min(areas)
+# print (m)
+# rnd=round(m,2)
+# print(rnd)
+# #help(len)
+# v =sorted(areas)#sorted helps retur a organised list from the list value to the highest value 
+# print(v)
+# ind=areas.index(20.0)
+# print(ind)
+# cnt= areas.count(20.0)
+# print(cnt)
+
+y = [18, 5, 2, 4]
+y.reverse()
+print(y)
